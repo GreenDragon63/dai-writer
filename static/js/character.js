171ca9f553236e1da373f6f8e2effc86ec201d11
Christophe <christophe@webmill.fr>
2023-06-01 08:09:41 +0000

    Upload character cards

--- static/js/character.js
+++ static/js/character.js
@@ -1,5 +1,36 @@
 import { CharacterComponent } from "./character-component.js"
 
+document.getElementById("upload-button").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    const fileInput = document.getElementById("upload-card");
+    if (fileInput.files.length === 0) {
+        alert("Please select a file.");
+        return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", fileInput.files[0]);
+
+    fetch("/api/upload", {
+        method: "POST",
+        body: formData
+    })
+    .then(response => {
+        if (response.ok) {
+            alert("Upload successful!");
+            document.getElementById("upload-card").value = "";
+            document.getElementById("fileName").textContent = "Choose File";
+        } else {
+            alert("Upload failed. Please choose a character card.");
+        }
+    })
+    .catch(error => {
+        alert("An error occurred. Please try again.");
+    });
+});
+
+
 fetch("/api" + window.location.pathname)
     .then(response => response.json())
     .then(data => {
